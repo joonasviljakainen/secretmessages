bf86f6ffc10f0c834fb3cfdcb52e22730b7a21cf
joonasviljakainen <joonasviljakainen@gmail.com>
2019-08-13 21:08:01 +0300

    updated documentation, pom.xml

--- secretmessages/src/main/java/Steganography/EHEncoding.java
+++ secretmessages/src/main/java/Steganography/EHEncoding.java
@@ -23,8 +23,17 @@ public class EHEncoding {
     private static final double DEFAULT_ECHO_AMPLITUDE = 0.5;
     private static final short SIGNAL_LIMIT_MAGNITUDE = 32760;
 
+    /**
+     * Encodes a message to an audio signal using Echo Hiding (EH).
+     *
+     * @param data The signal to which the message will be encoded. The data
+     * must be 16-bit PCM format. Input data can be a raw byte stream: this
+     * method will convert the data to 16-bit signed integers for encoding.
+     * @param message The message to hide as bytes. Only 8-bit (ASCII) symbols are supported.
+     * @return
+     */
     public static byte[] encode(byte[] data, byte[] message) {
-
+        // TODO add support for 16kHx and 8 kHz sampling rates
         // convert to something we can handle
         short[] pcmData = littleEndianByteArrayToShorts(data);
         // establish & check boundaries
@@ -48,19 +57,9 @@ public class EHEncoding {
 
     private static short[] convolveThreeSignals(short[] data, short[] zeroDelayed, short[] oneDelayed, double[] mixer, double decay) {
         for (int i = 0; i < mixer.length; i++) {
-            // Range checks to prevent overflow (overflow -> distortion, and 
-            // obviously we want the echo to be as unnoticeable as possible)
+            //calculating the magnitude of the total echo
             short echo = (short) ((decay * zeroDelayed[i] * (Mathematics.abs(mixer[i] - 1))) + (decay * oneDelayed[i] * mixer[i]));
-            //short echo;
-
-            /*if (mixer[i] == 0.0) {
-                echo = (short) (decay * zeroDelayed[i]);
-                zeroes++;
-            } else {
-                echo = (short) (decay * oneDelayed[i]);
-                count++;
-            }*/
-
+            // Range checks 
             if (data[i] > 0 && echo > 0) {
                 if (SIGNAL_LIMIT_MAGNITUDE - echo < data[i]) {
                     data[i] = SIGNAL_LIMIT_MAGNITUDE;
@@ -73,12 +72,25 @@ public class EHEncoding {
                 }
             }
             data[i] = (short) (data[i] + echo);
-
-            //data[i] = (short) (echo);
         }
         return data;
     }
 
+    /**
+     * Creates a mixer signal with a number of elements equal to the number of
+     * frames (essentially samples) in the data being EH encoded. The decimal
+     * numbers can be used for determining which echo (one or zero) should be
+     * embedded at a particular moment.
+     *
+     * In addition, its purpose is to smoothen the transition between the two
+     * echoes.
+     *
+     * @param message The message being hidden as bytes
+     * @param messageContainerLengthInFrames Number of frames
+     * @param numFramesForSingleBit Number of bits to be marked with a
+     * particular bit (essentially, how long the signal for each bit should be)
+     * @return The mixer signal with values varying from 0.0 to 1.0.
+     */
     private static double[] createMixerSignal(byte[] message, int messageContainerLengthInFrames, int numFramesForSingleBit) {
         double[] res = new double[messageContainerLengthInFrames];
 
@@ -124,8 +136,8 @@ public class EHEncoding {
      * @param delayAsFrames Number of frames to delay. E.g. if samples rate is
      * 44100 per second, giving the value 44100 will create a delay of one
      * second.
-     * @param decay Magnitude i.e. volume of the echo compared to the original. 1.0 stands for equal volume.
-     * signal.
+     * @param decay Magnitude i.e. volume of the echo compared to the original.
+     * 1.0 stands for equal volume. signal.
      * @return
      */
     public static short[] delay(short[] data, int delayAsFrames, double decay) {
@@ -134,8 +146,7 @@ public class EHEncoding {
             res[i] = data[i];
         }
         for (int i = delayAsFrames, a = 0; i < data.length; i++, a++) {
-            // Range checks to prevent overflow (overflow -> distortion, and 
-            // obviously we want the echo to be as unnoticeable as possible)
+            // Range checks to prevent overflow 
             double echo = decay * data[a];
             if (data[i] > 0 && echo > 0) {
                 if (SIGNAL_LIMIT_MAGNITUDE - echo < data[i]) {
